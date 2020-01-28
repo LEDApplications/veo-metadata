94f35a8992e499d717f040bf81e100c4c061496b
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-01-28 15:26:50 -0500

    create out dir if not exists and add current raw files

--- src/generate_metadata.py
+++ src/generate_metadata.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
+from pathlib import Path
 
-from format_simple_occ import format_simple_occ
 from format_detailed_occ import format_detailed_occ
+from format_simple_occ import format_simple_occ
 
 if __name__ == "__main__":
+    # format readable output
     pretty = True
+
+    # create output directory
+    Path("../out").mkdir(parents=True, exist_ok=True)
+
     print("Generating metadata files in the ../out directory")
     format_simple_occ(pretty)
-    #format_detailed_occ(pretty)
+    format_detailed_occ(pretty)
