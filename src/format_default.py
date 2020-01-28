7471c1729e52f5c4db579a297f4a0211010e0cee
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-01-28 15:41:27 -0500

    add module for default label handling

---
+++ src/format_default.py
@@ -0,0 +1,17 @@
+#!/usr/env/bin python
+
+from utils import read_csv, write_json
+
+
+def format_default(file_name, label_column, pretty=False):
+    print("Creating two digit occupation codes")
+    csv_file = '../raw/{}.csv'.format(file_name)
+    json_file = '../out/{}.json'.format(file_name)
+
+    csv_data = []
+    for i, row in enumerate(read_csv(csv_file)):
+        veo_id = row[label_column]
+        label = row['label']
+        csv_data.extend([{"id": veo_id, "label": label}])
+
+    write_json(csv_data, json_file, "labels", pretty)
--- src/generate_metadata.py
+++ src/generate_metadata.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from pathlib import Path
 
+from format_default import format_default
 from format_detailed_occ import format_detailed_occ
 from format_simple_occ import format_simple_occ
 
@@ -11,6 +12,10 @@ if __name__ == "__main__":
     # create output directory
     Path("../out").mkdir(parents=True, exist_ok=True)
 
+    # handling for custom logic of occ/mos labels
     print("Generating metadata files in the ../out directory")
     format_simple_occ(pretty)
     format_detailed_occ(pretty)
+
+    # handling for default layout
+    format_default("label_afqtgrp", "afqtgrp", pretty)
