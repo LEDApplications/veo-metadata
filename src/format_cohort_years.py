61ae710a6f69a0955573d0d51876528a7abecc5a
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-01-28 16:43:29 -0500

    format the cohort years

---
+++ src/format_cohort_years.py
@@ -0,0 +1,16 @@
+#!/usr/env/bin python
+
+from utils import write_json
+
+
+def format_cohort_years(start_year, end_year, years, pretty=False):
+    print("Creating {} year labels".format(years))
+    json_file = '../out/label_{}year_cohorts.json'.format(years)
+
+    csv_data = []
+    for x in range(start_year, end_year, years):
+        veo_id = "{}".format(x)
+        label = "{}-{}".format(x, x+years-1)
+        csv_data.extend([{"id": veo_id, "label": label}])
+
+    write_json(csv_data, json_file, "labels", pretty)
--- src/generate_metadata.py
+++ src/generate_metadata.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 from pathlib import Path
 
-from format_default import format_default
-from format_detailed_occ import format_detailed_occ
 from format_simple_occ import format_simple_occ
+from format_detailed_occ import format_detailed_occ
+from format_default import format_default
+from format_cohort_years import format_cohort_years
 
 if __name__ == "__main__":
+    # set year range for cohort labels
+    start_year = 2000
+    end_year = 2015
+
     # format readable output
     pretty = True
 
@@ -28,3 +33,8 @@ if __name__ == "__main__":
     format_default("label_race", "race", pretty)
     format_default("label_sex", "sex", pretty)
     format_default("label_yosgrp", "yosgrp", pretty)
+
+    # add custom year labels per cohort size
+    format_cohort_years(start_year, end_year, 2, pretty)
+    format_cohort_years(start_year, end_year, 4, pretty)
+    format_cohort_years(start_year, end_year, 8, pretty)
