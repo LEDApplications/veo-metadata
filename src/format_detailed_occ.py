00318f201fdb58f249b184a043379421bbc10e6a
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2020-11-04 15:58:04 -0500

    Bug: handle detailed_occ via monkeypatch

--- src/format_detailed_occ.py
+++ src/format_detailed_occ.py
@@ -6,6 +6,7 @@ from utils import read_csv, write_json
 def format_detailed_occ(pretty=None):
     print("Creating detailed occupation labels")
     csv_file = '../raw/label_dod_occ_code_w_mosc.csv'
+    csv_patch_file = '../raw/label_dod_occ_code.csv'
     json_file = '../out/metadata/label_dod_occ_code_detailed.json'
     json_file_rollup = '../out/metadata/label_dod_occ_code_rollup.json'
 
@@ -37,14 +38,27 @@ def format_detailed_occ(pretty=None):
                     if mos_two not in mos_rollup_dict:
                         mos_rollup_dict[mos_two] = mos_dict[mos_three]
                     else:
-                        mos_rollup_dict[mos_two] = "{}, {}".format(mos_rollup_dict[mos_two], mos_dict[mos_three])
+                        mos_rollup_dict[mos_two] = "{}, {}".format(
+                            mos_rollup_dict[mos_two], mos_dict[mos_three])
 
     # patch the mos_dict with the updated rollup values
     for key in mos_rollup_dict.keys():
         mos_dict[key] = mos_rollup_dict[key]
 
+    # output data
     csv_data = []
     csv_data_rollup = []
+
+    # monkey patch the all jobs '000' rollup from the label_dod_occ_code
+    for i, row in enumerate(read_csv(csv_patch_file)):
+        if 'A' in row['dod_occ_code_level']:
+            dod_id = row['dod_occ_code']
+            label = row['label']
+            csv_data.extend([{'id': dod_id, 'label': label, 'details': label}])
+            csv_data_rollup.extend(
+                [{'id': dod_id, 'label': label, 'details': label}])
+
+    # fill out standard occ data
     for key in occ_dict.keys():
         if key not in mos_dict:
             print("ERROR: no mos code found for {}".format(key))
@@ -56,7 +70,8 @@ def format_detailed_occ(pretty=None):
         if 'X' not in ident:
             csv_data.extend([{'id': ident, 'label': label, 'details': details}])
         else:
-            csv_data_rollup.extend([{'id': ident, 'label': label, 'details': details}])
+            csv_data_rollup.extend(
+                [{'id': ident, 'label': label, 'details': details}])
 
     write_json(csv_data, json_file, "labels", pretty)
     write_json(csv_data_rollup, json_file_rollup, "labels", pretty)
